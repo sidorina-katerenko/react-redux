da9eb42d7ff8fc9571ada8ed93e9ae1968a0381a
kate <ksidorenko@3knbh.com>
2022-10-02 17:15:44 +0100

    Introduction to Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index";
 
 function App() {
 	const counter = useSelector((state) => state.counter);
 	const dispatch = useDispatch();
 
 	const increment = () => {
-		dispatch({ type: "INC" });
+		dispatch(actions.increment());
 	};
 
 	const decrement = () => {
-		dispatch({ type: "DEC" });
+		dispatch(actions.decrement());
 	};
 
 	const addBy = () => {
-		dispatch({ type: "ADD", payload: 10 });
+		dispatch(actions.addBy(10));
 	};
 
 	return (
